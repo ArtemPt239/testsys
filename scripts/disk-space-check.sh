d2ceb652a6d9a76b63987b8efe63a597f1815371
Artem <a99p@truekoo.com>
2017-09-03 11:08:09 +0300

    disk check fixed :x

--- scripts/disk-space-check.sh
+++ scripts/disk-space-check.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # check if there are at least 30% of disk-space available in each file system
 myout=`df`
-if printf "%s" "$myout" | sed '/Filesystem          1K-blocks    Used Available Use% Mounted on/d' | sed 's/%//' | awk 'BEGIN {f = 0;} { if( $5 >= 70) {f = 1;} } END {if(f == 1) exit 0; else exit 1;}' ;   
+if printf "%s" "$myout" | sed '/Filesystem/d' | sed 's/%//' | awk 'BEGIN {f = 0;} { if( $5 >= 70) {f = 1;} } END {if(f == 1) exit 0; else exit 1;}' ;   
 then
   echo 'DISK-SPACE-CHECK FAILED:';
   printf "%s\n" "$myout";
