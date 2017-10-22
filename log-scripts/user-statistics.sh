ee26269af143a032d7a0c6fa9d47bad8dc7a3002
root <root@mail.skyratio.com>
2017-10-22 21:37:16 +0300

    upgrede of userstat

--- log-scripts/user-statistics.sh
+++ log-scripts/user-statistics.sh
@@ -21,9 +21,13 @@ echo "Total logins  :" `cat $_path_userlogins |  grep $_user | grep "Successful 
 
 echo "failed logins :" `cat $_path_userlogins | grep $_user | grep "Failed login for"  | sed 's/.*Failed login for //' | awk '{ print($1) }' | wc -l`
 
-echo "recieved mails:" `cat /var/log/maillog | grep "to=<"$_user">"  | grep "postfix/smtp" | wc -l `
+echo "recieved mails:" `cat /var/log/maillog* | grep "to=<"$_user">"  | grep "postfix/smtp" | wc -l `
 
-echo "sent mails    :" `cat $_path_sendmail | grep "User "$_user | wc -l`
+sent_counter=$((`cat /var/log/maillog* | grep "from=<"$_user">"  | wc -l ` / 2))
+
+echo "sent mails    :" $sent_counter
+
+echo "sent mails(since the beginnig of the world):" `cat $_path_sendmail | grep "User "$_user | wc -l`
 
 echo "successful logins per ip:"
 cat $_path_userlogins | grep $_user | grep "Successful login for"  | sed 's/.*Successful login for //' | awk '{ print($1 " " $5) }' | uniq -c | sort -nr
@@ -32,7 +36,7 @@ echo "failed logins per ip:"
 cat $_path_userlogins | grep $_user | grep "Failed login for"  | sed 's/.*Failed login for //' | awk '{ print($1 " " $3) }' | uniq -c | sort -nr
 
 echo --------------------------
-echo "(recieved mails calculated since last logrotation)"
+echo "(recieved and sent mails calculated since last logrotation)"
 else
 echo "Need to specify username"
 fi
