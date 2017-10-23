fee64e78b89091d510e00df9a1df804f9c93ac9e
root <root@truekoo.com>
2017-10-23 21:17:02 +0000

    fixed cpu-usage 2nd attempt to fix it

--- scripts/cpu-usage-check.sh
+++ scripts/cpu-usage-check.sh
@@ -5,7 +5,7 @@ minutes=`date +%M`
 # Since sar is not working correctly a few minutes after midnight we would assume that everything is ok without checking it
 if [ \( $hours = '00' \) -a \( $minutes -lt 19 \) ]
 then
-  echo 10;
+  echo 0;
 else
   myout=`LC_TIME=en_UK.utf8 sar -u | tail -5 | head -4 `
   if [ `printf "%s\n" "$myout" | awk '{s+=$8;} END {printf("%f\n",s/4);}'` \< 20 ]; then 
